ed620864db3345a703279e0dea44f71b7c97a72f
Ajithkumar-FullStack-Developer <ajithjana1996@gmail.com>
2024-06-30 13:03:34 +0530

    day6

---
+++ Day6/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+//Map, reduce, Filter
+
+//Using MAP
+var scores = [3, 5, 7, 1, 9,];
+function callBack(element, index, self){
+    return = element * 2;
+}
+var out = scores.map(callBack);
+console.log(map);
