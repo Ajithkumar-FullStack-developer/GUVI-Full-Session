9c2e5db40cd07f238c56cd87305ccb0ff8ecee37
Ajithkumar-FullStack-Developer <ajithjana1996@gmail.com>
2024-07-05 13:13:59 +0530

    day3Task

--- Task/Day3/AnonymousFunction&IIFE.js
+++ Task/Day3/AnonymousFunction&IIFE.js
@@ -31,3 +31,60 @@ printOddNumbers(numbers);
     });
 })
 ([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+/**
+Anonymous Function & IIFE
+2. Convert all the strings to title caps in a string array
+*/
+//Anonymous Funtion
+
+const lowerToUpper = function(arr)
+{
+    const result = [];
+    for (let i = 0; i < arr.length; i++)
+        {
+        const words = arr[i].toLowerCase().split(' ');
+        for (let j = 0; j < words.length; j++)
+        {
+            words[j] = words[j].charAt(0).toUpperCase() + words[j].slice(1);
+        }
+        result.push(words.join(' '));
+    }
+    return result;
+};
+
+const strings = ["welcome to guvi", "hello student", "programmer"];
+const stringTitleCase = lowerToUpper(strings);
+console.log(stringTitleCase);
+
+//Immediately Invoked Function Expression
+
+const textTitleCase = (function(arr)
+{
+    const result = [];
+    for (let i = 0; i < arr.length; i++)
+        {
+        const words = arr[i].toLowerCase().split(' ');
+        for (let j = 0; j < words.length; j++)
+            {
+            words[j] = words[j].charAt(0).toUpperCase() + words[j].slice(1);
+        }
+        result.push(words.join(' '));
+    }
+    return result;
+})(["welcome to guvi", "hello student", "programmer"]);
+
+console.log(textTitleCase);
+
+/**
+Anonymous Function & IIFE
+3. Sum of all numbers in an array
+*/
+//Anonymous Funtiona
+
+let sumArray = function(arr) {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+};
+
+console.log(sumArray([1, 2, 3, 4, 5]));
+
