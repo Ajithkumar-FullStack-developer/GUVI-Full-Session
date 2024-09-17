13f4ea176d2923c552fc243b1d8a27b2aa54a622
Ajithkumar-FullStack-Developer <ajithjana1996@gmail.com>
2024-09-17 06:57:30 +0530

    Day 19

---
+++ Day19/script.js
@@ -0,0 +1,15 @@
+const JSON = {
+    "tasks":[
+        {
+            "task":"Get up at 4.00AM"
+        },
+        {
+            "task":"Brush teeth"
+        },
+        {
+            "task":"Lets learn Javascript"
+        }
+    ],
+    "message":"Tasks fetched successfully!"
+}
+const 
